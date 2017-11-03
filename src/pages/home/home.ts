cd5a950508578a76237e3ce96699e32f8bff2df6
diontanjung <dae.one11@gmail.com>
2017-11-03 16:48:31 +0700

    home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,21 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    
+  }
 
+  /* go to menu page
+    @var index => index menu
+  */
+  openMenu(index){
+    if (index==1){
+      this.navCtrl.push('WorkoutPage');
+    }else if(index==2){
+      this.navCtrl.push('HistoryPage'); 
+    }else{
+      this.navCtrl.push('ReportPage');
+      
+    }
   }
 
 }
